1a291a550f2d2b210c62f501c5ae030bcdd6a3a9
Losersock <lachlan_jamieson@outlook.com>
2024-08-23 19:23:57 +0200

    Add files via upload

---
+++ TowerDefenceGame/Scripts/turret_data.py
@@ -0,0 +1,22 @@
+TURRET_DATA = [
+  {
+    #1
+    "range": 90,
+    "cooldown": 1000,
+  },
+  {
+    #2
+    "range": 90,
+    "cooldown": 500,
+  },
+  {
+    #3
+    "range": 135,
+    "cooldown": 500,
+  },
+  {
+    #4
+    "range": 135,
+    "cooldown": 333,
+  }
+]
